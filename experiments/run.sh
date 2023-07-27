9f7653c4d22b9255eae083c634e5ba8cfdf67b31
TomQuartz <shengqi2018@pku.edu.cn>
2023-07-27 04:43:35 -0500

    switch to ap-southeast-1a; nits

--- experiments/run.sh
+++ experiments/run.sh
@@ -2,15 +2,12 @@
 BASE_DIR=`realpath $(dirname $0)`
 cd $BASE_DIR
 
-cd $BASE_DIR/workflow
-./run_quick.sh > workflow.log 2>&1
-
-echo "Finished workflow"
-
 cd $BASE_DIR/singleop
-./run_quick.sh > singleop.log 2>&1
+./run_quick.sh 1 >run.log 2>&1
 
 echo "Finished singleop"
 
-cd $BASE_DIR/logsize
-./optimal/run_all.sh 2 > logsize.log 2>&1
+cd $BASE_DIR/workflow
+./run_quick.sh 1 >run.log 2>&1
+
+echo "Finished workflow"
