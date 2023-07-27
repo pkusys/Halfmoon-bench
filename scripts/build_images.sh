46a54ca80e3d142a307e27151df1a8c96bb816d3
TomQuartz <shengqi2018@pku.edu.cn>
2023-07-27 07:52:17 -0500

    tidy dockerfiles

---
+++ scripts/build_images.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -exu
+
+ROOT_DIR=`realpath $(dirname $0)/..`
+# TAG=single-reorder
+TAG=sosp-ae
+
+# Use BuildKit as docker builder
+export DOCKER_BUILDKIT=1
+
+function build_halfmoon {
+    docker build -t shengqipku/halfmoon:$TAG \
+        -f $ROOT_DIR/dockerfiles/Dockerfile.Halfmoon \
+        $ROOT_DIR/boki
+}
+
+function build_halfmoon_bench {
+    docker build -t shengqipku/halfmoon-bench:$TAG \
+        -f $ROOT_DIR/dockerfiles/Dockerfile.Halfmoon-bench \
+        $ROOT_DIR/workloads/workflow
+}
+
+function build {
+    build_halfmoon
+    build_halfmoon_bench
+}
+
+function push {
+    docker push shengqipku/halfmoon:$TAG
+    docker push shengqipku/halfmoon-bench:$TAG
+}
+
+case "$1" in
+build)
+    build
+    ;;
+push)
+    push
+    ;;
+esac
