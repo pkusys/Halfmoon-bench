840797e413060c029b911cae0b2a570612bc76a3
TomQuartz <shengqi2018@pku.edu.cn>
2023-07-29 07:16:26 -0500

    add auto retry for baseline and boki workflow

--- experiments/workflow/boki-hotel/run_all.sh
+++ experiments/workflow/boki-hotel/run_all.sh
@@ -13,10 +13,27 @@ QPS=(100 300 500 700)
 $HELPER_SCRIPT start-machines --base-dir=$BASE_DIR --instance-iam-role=$BOKI_MACHINE_IAM
 
 for qps in ${QPS[@]}; do
+    # EXP_DIR=QPS${qps}
+    # $BASE_DIR/run_once.sh $EXP_DIR $qps # 2>&1 | tee run.log 
+    # mv $BASE_DIR/results/$EXP_DIR $BASE_DIR/results/${EXP_DIR}_$RUN
+    # echo "finished QPS${qps}"
     EXP_DIR=QPS${qps}
-    $BASE_DIR/run_once.sh $EXP_DIR $qps # 2>&1 | tee run.log 
-    mv $BASE_DIR/results/$EXP_DIR $BASE_DIR/results/${EXP_DIR}_$RUN
-    echo "finished QPS${qps}"
+    if [ -d "$BASE_DIR/results/${EXP_DIR}_$RUN" ]; then
+        echo "finished QPS${qps}"
+        continue
+    fi
+    while true; do
+        sleep 60
+        $BASE_DIR/run_once.sh $EXP_DIR $qps # 2>&1 | tee run.log 
+        if [ -s "$BASE_DIR/results/$EXP_DIR/async_results" ]; then
+            mv $BASE_DIR/results/$EXP_DIR $BASE_DIR/results/${EXP_DIR}_$RUN
+            echo "finished QPS${qps}"
+            break
+        else
+            echo "retrying QPS${qps}"
+            rm -rf $BASE_DIR/results/$EXP_DIR
+        fi
+    done
 done
 
 $HELPER_SCRIPT stop-machines --base-dir=$BASE_DIR
